51ae4090bba5a4f765ce86affd51b6f911330133
Alejandra Barros <alejandra.barros@aluno.senai.br>
2025-03-06 11:21:51 -0300

    feat: create login page

---
+++ app/page.jsx
@@ -0,0 +1,34 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.background}>
+            <div className={styles.container}>
+                <div className={styles.article}>
+                    <h1>WELCOME!</h1>
+                    <h3>Join us in this journey</h3>
+                </div>
+                <div className={styles.aside}>
+                    <h1>LOGIN</h1>
+                    <input type="text" placeholder="E-mail Address or Username" />
+                    <input type="password" placeholder="Password" />
+                    <button type="submit">CONTINUE</button>
+                    <p>Connect with</p>
+                    <div className={styles.socialmedia}>
+                        <div className={styles.media}>
+                            <img src="/google-icon.png"/>
+                        </div>
+                        <div className={styles.media}>
+                            <img src="/ios-icon.png"/>
+                        </div>
+                        <div className={styles.media}>
+                            <img src="/facebook-icon.png"/>
+                        </div>
+                    </div>
+                    <p>Or</p>
+                    <h2>SIGN UP!</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
